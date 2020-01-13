1483dd0fae3043190d2184427ed9b4bbab3269bc
Stephen Schloesser <sschloesser2@gmail.com>
2020-01-13 22:30:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'dog'
+var animal = 'cat'
 
 function myAnimal() {
   return animal
@@ -12,10 +12,7 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+  return n + 2
 }
 
 var funkyFunction = function() {
