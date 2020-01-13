a4bf7d193b20c0bd673fa1abcbaec6e257a65449
Stephen Schloesser <sschloesser2@gmail.com>
2020-01-13 22:34:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  if(animal == 'dpg'){
+  if(animal == 'dog'){
     animal = 'cat'
   }
   
